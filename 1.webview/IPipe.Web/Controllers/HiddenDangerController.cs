8f21efca923c77f8e61c8c0fbc320b15b5a9b8ff
GodProgrammerMan <960842214@qq.com>
2020-06-08 11:34:57 +0800

    1

--- 1.webview/IPipe.Web/Controllers/HiddenDangerController.cs
+++ 1.webview/IPipe.Web/Controllers/HiddenDangerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using IPipe.IServices;
+using IPipe.Model.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IPipe.Web.Controllers
@@ -17,12 +18,26 @@ namespace IPipe.Web.Controllers
             _ihidden_DangerServices = ihidden_DangerServices;
             _ipipe_LineServices = ipipe_LineServices;
         }
-        public IActionResult Index(string action="add",int ty=1,double x=0,double y=0,double z=0,int objID=0)
+        public IActionResult Index(string action="add",int ty=1,double x=0,double y=0,double z=0,int objID=0,int id=0)
         {
             //创建和编辑隐患
+            hidden_danger model = new hidden_danger();
+            if (objID == 0 || "add".Equals(action) || !_ihidden_DangerServices.IsAny(t => t.id == id))
+            {
+                model = null;
+                action = "add";
+                ViewBag.departmentName = "无部门";
+                ViewBag.departmentID = 0;
+            }
+            else
+            {
+                model = _ihidden_DangerServices.Query(t => t.id == id).Result.First();
+                action = "edit";
+            }
+            //固定添加的参数附上
 
-
-            return View();
+            ViewBag.action = action;
+            return View(model);
         }
 
     }
